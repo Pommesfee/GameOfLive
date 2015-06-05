a542c8e7220fdbd8a9929303af1fd59e8c610c91
Pommesfee <klook.christian@gmail.com>
2015-06-05 08:29:16 +0200

    Fixed classpath setup.

--- GameOfLive/src/core/GameOfLife.java
+++ GameOfLive/src/core/GameOfLife.java
@@ -29,7 +29,7 @@ public class GameOfLife {
 				}
 
 				@SuppressWarnings("unused")
-				JPanel frame = new Frame(750, 750, "Game Of Live", 50, 50, 50);
+				JPanel frame = new Frame(750, 750, "Game Of Live", 8, 8, 50);
 			}
 
 		});
